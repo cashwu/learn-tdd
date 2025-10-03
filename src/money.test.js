655da5a620b9d67123e4b9990a4a82fe4d8409da
Cash <cashwugeek@icloud.com>
2025-10-03 16:46:50 +0800

    [refactor] reformat code

--- src/money.test.js
+++ src/money.test.js
@@ -1,29 +1,29 @@
 const Money = require("./money");
 
 describe("Money", () => {
-  beforeEach(() => {
-    // Setup code here
-  });
+    beforeEach(() => {
+        // Setup code here
+    });
 
-  afterEach(() => {
-    // Cleanup code here
-  });
+    afterEach(() => {
+        // Cleanup code here
+    });
 
-  it("5 usd x 2 = 10 usd", () => {
-    let five = new Money(5, "USD");
-    let ten = new Money(10, "USD");
-    expect(five.times(2)).toStrictEqual(ten);
-  });
+    it("5 usd x 2 = 10 usd", () => {
+        let five = new Money(5, "USD");
+        let ten = new Money(10, "USD");
+        expect(five.times(2)).toStrictEqual(ten);
+    });
 
-  it("10 eur x 2 = 20 eur", () => {
-    let tenEuros = new Money(10, "EUR");
-    let twenty = new Money(20, "EUR");
-    expect(tenEuros.times(2)).toStrictEqual(twenty);
-  });
+    it("10 eur x 2 = 20 eur", () => {
+        let tenEuros = new Money(10, "EUR");
+        let twenty = new Money(20, "EUR");
+        expect(tenEuros.times(2)).toStrictEqual(twenty);
+    });
 
-  it("4002 krw / 4 = 1000.5 krw", () => {
-    let originalMoney = new Money(4002, "krw");
-    let expectedMoneyAfterDivision = new Money(1000.5, "krw");
-    expect(originalMoney.divide(4)).toStrictEqual(expectedMoneyAfterDivision);
-  });
+    it("4002 krw / 4 = 1000.5 krw", () => {
+        let originalMoney = new Money(4002, "krw");
+        let expectedMoneyAfterDivision = new Money(1000.5, "krw");
+        expect(originalMoney.divide(4)).toStrictEqual(expectedMoneyAfterDivision);
+    });
 });
