0f57e58dbf0d0f9723421085046269344d543cb2
Cash <cashwugeek@icloud.com>
2025-10-03 16:00:11 +0800

    [red] 10 eur x 2 = 20 eur

--- src/money.test.js
+++ src/money.test.js
@@ -9,9 +9,16 @@ describe("Money", () => {
     // Cleanup code here
   });
 
-  it("should create a new instance", () => {
+  it("5 usd x 2 = 10 usd", () => {
     let fiver = new Dollar(5);
     let tenner = fiver.times(2);
     expect(tenner.amount).toBe(10);
+  });
+
+  it("10 eur x 2 = 20 eur", () => {
+    let tenEuros = new Dollar(10, "EUR");
+    let twentyEuros = tenEuros.times(2);
+    expect(twentyEuros.amount).toBe(20);
+    expect(twentyEuros.currency).toBe("EUR");
   });
 });
