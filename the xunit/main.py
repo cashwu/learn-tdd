7eb17e7c8ba7dceff5fb975f08c28b4b2da7a9b1
cash <cashwugeek@icloud.com>
2025-10-10 20:40:21 +0800

    [ch20] remove setup

--- the xunit/main.py
+++ the xunit/main.py
@@ -29,17 +29,12 @@ class WasRun(TestCase):
 
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
 
     def testTemplateMethod(self):
-        self.test.run()
-        assert ("setUp testMethod " == self.test.log)
+        test = WasRun("testMethod")
+        test.run()
+        assert ("setUp testMethod " == test.log)
 
 
 if __name__ == '__main__':
     TestCaseTest("testTemplateMethod").run()
-    # test = WasRun("testMethod")
-    # print(test.wasRun)
-    # test.run()
-    # print(test.wasRun)
