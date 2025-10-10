cf9837a4a32f74335719f105386c720e978ee6b3
cash <cashwugeek@icloud.com>
2025-10-10 21:18:31 +0800

    [ch23] implement TestSuit run

--- the xunit/main.py
+++ the xunit/main.py
@@ -52,6 +52,12 @@ class TestSuit:
     def add(self, test):
         self.tests.append(test)
 
+    def run(self):
+        result = TestResult()
+        for test in self.tests:
+            test.run(result)
+        return result
+
 
 class TestCaseTest(TestCase):
 
