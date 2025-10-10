23dde65999860768c818f939d92d11a32903a804
cash <cashwugeek@icloud.com>
2025-10-10 20:38:57 +0800

    [ch20] remove testRunning and rename testSetup to testTemplateMethod

--- the xunit/main.py
+++ the xunit/main.py
@@ -32,17 +32,13 @@ class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
 
-    def testRunning(self):
-        self.test.run()
-        assert (self.test.wasRun)
-
-    def testSetUp(self):
+    def testTemplateMethod(self):
         self.test.run()
         assert ("setUp testMethod " == self.test.log)
 
 
 if __name__ == '__main__':
-    TestCaseTest("testSetUp").run()
+    TestCaseTest("testTemplateMethod").run()
     # test = WasRun("testMethod")
     # print(test.wasRun)
     # test.run()
