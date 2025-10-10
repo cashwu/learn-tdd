32e29bca93ff01e8943f60817362c55e9f4575eb
cash <cashwugeek@icloud.com>
2025-10-10 17:21:23 +0800

    [ch18] pull up run method to TestCase

--- the xunit/main.py
+++ the xunit/main.py
@@ -2,16 +2,16 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
 
 class WasRun(TestCase):
 
     def __init__(self, name):
-        self.name = name
+        self.wasRun = None
         TestCase.__init__(self, name)
-
-    def run(self):
-        method = getattr(self, self.name)
-        method()
 
     def testMethod(self):
         self.wasRun = 1
