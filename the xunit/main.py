0cbde7a3390441cb75f4d75b5d8c72c87726964c
cash <cashwugeek@icloud.com>
2025-10-10 17:19:53 +0800

    [ch18] pull up attr name to TestCase

--- the xunit/main.py
+++ the xunit/main.py
@@ -1,11 +1,13 @@
 class TestCase:
-    pass
+    def __init__(self, name):
+        self.name = name
 
 
 class WasRun(TestCase):
+
     def __init__(self, name):
-        self.wasRun = None
         self.name = name
+        TestCase.__init__(self, name)
 
     def run(self):
         method = getattr(self, self.name)
